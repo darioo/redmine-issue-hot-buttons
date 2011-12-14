438970831ea130d2b3a72835bcf2c13019efe511
Mike Kolganov <mike.kolganov@gmail.com>
2011-12-15 04:05:27 +0700

    Plugin settings page completed

--- assets/javascripts/settings.js
+++ assets/javascripts/settings.js
@@ -1,55 +1,91 @@
 document.observe("dom:loaded", function() {
+
 	function init_sortable_list() {
 		Sortable.create('buttons_list', {
 			tag:'li',
-			onChange: function(){
-				console.log(Math.random());
-			}
+			onChange: function(){}
 		});
 	}
-	
-	function add_hot_button(event) {
+
+  function init_saved_buttons() {
+    if ('undefined' != typeof settings) {
+      for (var button_number in settings) {
+        var button = settings[button_number];
+        for (var button_name in button) {
+          var button_options = button[button_name];
+          add_hot_button_to_list(button_name, button_options);
+        }
+      }
+    }
+  }
+
+  function add_hot_button_to_list(button_name, button_options) {
+		var button_element = $$('#buttons_repository li[button_id="' + button_name + '"]');
+		if (0 == button_element.length) return false;
+
+		button_element = button_element[0];
+
+		if (button_element.hasClassName('unique')) {
+			var exists = $$('#buttons_list li[button_id="' + button_name + '"]').length != 0;
+			if (exists) return false;
+
+		}
+
+    $('buttons_list').insert(button_element.clone(true));
+
+    if ('undefined' != typeof button_options) {
+      for (var key in button_options) {
+        var value = button_options[key];
+
+
+        var selector = [
+          '#buttons_list li[button_id="' + button_name + '"] input[sname="' + key + '"]',
+          '#buttons_list li[button_id="' + button_name + '"] textarea[sname="' + key + '"]'
+        ].join(',');
+
+        input_element = $$(selector);
+        if (input_element.length > 0) {
+          input_element = input_element[0];
+          input_element.value = value;
+        }
+      }
+    }
+
+    $$('.delete_hot_button').invoke('observe', 'click', delete_hot_button.bindAsEventListener('compare', this));
+    init_sortable_list();
+  }
+
+	function add_hot_button_click(event) {
 		var button_name = $$('#hot_button_select')[0].value;
 		if (0 == button_name.length) return false;
 		
-		var button_element = $$('#buttons_repository li[button_id="' + button_name + '"]');
-		if (0 == button_element.length) return false;
-		
-		button_element = button_element[0];
-
-		if (button_element.hasClassName('unique')) {		
-			var exists = $$('#buttons_list li[button_id="' + button_name + '"]').length != 0;
-			if (exists) return false;
-			
-		}
-		$('buttons_list').insert(button_element.clone(true));
-
-		$$('.delete_hot_button').invoke('observe', 'click', delete_hot_button.bindAsEventListener('compare', this)); 
-		init_sortable_list();
+    add_hot_button_to_list(button_name);
 	}
 	
 	function delete_hot_button(event) {
 		var element = Event.element(event);
 		try {
 			element.up(1).remove(true);
-		}catch(e){}
+		} catch(e) {}
 	}
 	
 	function update_inputs(event) {
+    var hot_button_number = 0;
 		$$('#buttons_list li').each(function(li){
 			var button_id = li.readAttribute('button_id');
-			$(li).select('input').each(function(input){
-				var name = 'settings[' + button_id + ']' + input.readAttribute('name_suffix')
-				console.log(name);
+			$(li).select('input, textarea').each(function(input){
+				var name = 'settings[' + hot_button_number + '][' + button_id + '][' + input.readAttribute('sname') + ']';
+				input.setAttribute('name', name);
 			});
+      hot_button_number++;
 		});
-
-		event.preventDefault();
 	}
 	
-	$('add_hot_button').observe('click', add_hot_button);
+	$('add_hot_button').observe('click', add_hot_button_click);
 	$$('.delete_hot_button').invoke('observe', 'click', delete_hot_button.bindAsEventListener('compare', this));
 	$$('input[name="commit"]')[0].observe('click', update_inputs);
+
 	init_sortable_list();
+  init_saved_buttons();
 
 });
