f75b10ce4abb45d5e2cc806b24ce269659272a05
Mike Kolganov <mike.kolganov@gmail.com>
2012-01-12 00:38:21 +0700

    Input mirroring implemented

--- assets/javascripts/hot_buttons.js
+++ assets/javascripts/hot_buttons.js
@@ -59,7 +59,9 @@ document.observe('dom:loaded', function(){
         });
       }
       else {
-        Event.observe(button, 'click', this.hot_button_action);
+        Event.observe(button, 'click', function(event) {
+          t.hot_button_submit_action(event, t);
+        });
       }
 
       return button;
@@ -70,12 +72,13 @@ document.observe('dom:loaded', function(){
         btn.removeAttribute('disabled');
         btn.setStyle({opacity: 1});
       });
-
-      var element = Event.element(event);
-      element.up().remove();
-    },
-
-    hot_button_action: function(event){
+      if (event) {
+        var element = Event.element(event);
+        element.up(1).remove();
+      }
+      else {
+        $('issue_hot_buttons_additional').remove();
+      }
     },
 
     hot_button_opt_action: function(event, t) {
@@ -88,10 +91,23 @@ document.observe('dom:loaded', function(){
 
       hot_button.setStyle({opacity: 1});
 
-      var additional_wrapper = new Element('span', {
+      var submit_button = new Element('button', {'class': 'submit'})
+        .insert(t._('submit'));
+      submit_button.config = hot_button.config;
+      Event.observe(submit_button, 'click', function(event) {
+        t.hot_button_submit_action(event, t);
+        t.hide_optional();
+      });
+      
+      var additional_wrapper = new Element('div', {
         'class': 'optional_wrapper'
-      })
-        .insert(new Element('button').insert(t._('submit')));
+      });
+
+      t.get_opt_controls(hot_button.config).each(function(element) {
+        additional_wrapper.insert(element);
+      });
+
+      additional_wrapper.insert(submit_button);
 
       var close_button = new Element('a', {
         'class': 'icon_close',
@@ -102,10 +118,93 @@ document.observe('dom:loaded', function(){
       var additional_container = new Element('div', {
         id: 'issue_hot_buttons_additional'
       })
-        .insert(close_button)
-        .insert(additional_wrapper);
+        .insert(new Element('div', {'class': 'controls'}).insert(close_button))
+        .insert(additional_wrapper)
+        .insert(new Element('div', {'class': 'clear'}));
 
       $('issue_hot_buttons').insert({after: additional_container});
+    },
+
+    get_opt_controls: function(button_config) {
+      t = this;
+      var elements = [];
+      button_config.keys().each(function(field){
+        switch(field) {
+
+          case 'assign_to_other':
+            var assign_to_roles = button_config.get('assign_to_other').evalJSON();
+            var mirrored_element = t.get_mirrored_element('issue_assigned_to_id');
+
+            var allowed_users = [];
+            assign_to_roles.each(function(role_id){
+              allowed_users = allowed_users.concat(t.users_per_role[role_id]);
+            });
+
+            mirrored_element.select('select option').each(function(option){
+              value = parseInt(option.readAttribute('value'));
+              if (0 > allowed_users.indexOf(value)) {
+                option.remove();
+              }
+            });
+
+            $('issue_assigned_to_id').value = mirrored_element.select('select').first().value;
+            elements.push(mirrored_element);
+            break;
+
+          case 'include_standart_fields':
+            var standart_fields = button_config.get('include_standart_fields').evalJSON();
+            standart_fields.each(function(standart_field){
+              elements.push(t.get_mirrored_element(standart_field));
+            });
+            break;
+
+          case 'include_custom_fields':
+            var custom_fields = button_config.get('include_custom_fields').evalJSON();
+            custom_fields.each(function(custom_field_id) {
+              elements.push(
+                t.get_mirrored_element(['issue_custom_field_values', custom_field_id].join('_'))
+              );
+            });
+            break;
+
+          case 'include_comment':
+            break;
+
+        }
+      });
+      return elements;
+    },
+
+    get_mirrored_element: function(element_id) {
+      mirror_element_id = ['hot_button', element_id].join('_');
+
+      var original_element = $(element_id);
+      if (! original_element) return false;
+
+      var mirrored_element = original_element.up().clone(true);
+      mirrored_element.select('input, select').first().writeAttribute('id', mirror_element_id);
+      mirrored_element.select('label').first().writeAttribute('for', mirror_element_id);
+
+      var calendar_field = mirrored_element.select('img.calendar-trigger').first();
+      if (! Object.isUndefined(calendar_field)) {
+        calendar_field.writeAttribute('id', [mirror_element_id, 'trigger'].join('_'));
+        Calendar.setup({
+          inputField : mirrored_element.select('input').first(),
+          ifFormat : '%Y-%m-%d',
+          button : calendar_field
+        });
+      }
+
+      /*Event.observe(mirrored_element.select('input,select').first(), 'change', function(event) {
+        original_element.value = Event.element(event).value;
+      });*/
+
+      return mirrored_element;
+    },
+
+    hot_button_submit_action: function(event, t){
+      var hot_button = Event.element(event);
+      console.log(hot_button.config)
     }
 
   });
